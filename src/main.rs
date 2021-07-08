4a52b3b7ba21b24667b289d0ae5bb2695c38b54d
Philip Kristoffersen <philipkristoffersen@gmail.com>
2021-07-08 06:50:29 +0200

    Center & Style remaining text

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod pie;
 use pie::Pie;
 use std::{env, io};
+use tui::layout::Alignment;
 use tui::Terminal;
 use tui::{
     backend::CrosstermBackend,
@@ -17,6 +18,39 @@ use std::time::{Duration, SystemTime};
 use rodio::Source;
 use std::fs::File;
 use std::io::BufReader;
+
+fn get_text_color(workmode: bool, minutes_remaining: i32) -> Option<Color> {
+    match (workmode, minutes_remaining) {
+        (true, x) if x <= 4 => Some(Color::Red),
+        (false, x) if x <= 0 => Some(Color::Green),
+        _ => None,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_text_color_none() {
+        assert_eq!(get_text_color(true, 20), None);
+    }
+
+    #[test]
+    fn test_get_text_color_red() {
+        assert_eq!(get_text_color(true, 4), Some(Color::Red));
+    }
+
+    #[test]
+    fn test_get_text_color_none_break() {
+        assert_eq!(get_text_color(false, 1), None);
+    }
+
+    #[test]
+    fn test_get_text_color_green() {
+        assert_eq!(get_text_color(false, 0), Some(Color::Green));
+    }
+}
 
 fn main() -> Result<(), io::Error> {
     let args: Vec<String> = env::args().collect();
@@ -37,18 +71,21 @@ fn main() -> Result<(), io::Error> {
             let elapsed_seconds = now.elapsed().unwrap().as_secs_f32();
             let remaining_seconds = total_time_seconds - elapsed_seconds;
             let percent = elapsed_seconds / total_time_seconds;
-            let minutes_remaining = (remaining_seconds / 60.) as i32;
-            let seconds_remaining = remaining_seconds as i32 % 60;
 
             let chunks = Layout::default()
                 .direction(Direction::Vertical)
                 .margin(1)
-                .constraints([Constraint::Percentage(90), Constraint::Percentage(10)].as_ref())
+                .constraints([Constraint::Percentage(95), Constraint::Percentage(5)].as_ref())
                 .split(f.size());
-            let span = Paragraph::new(text::Span::raw(format!(
-                "{}:{}",
-                minutes_remaining, seconds_remaining
-            )));
+
+            let minutes_remaining = (remaining_seconds / 60.) as i32;
+            let seconds_remaining = remaining_seconds as i32 % 60;
+            let mut time_text =
+                text::Span::raw(format!("{}:{}", minutes_remaining, seconds_remaining));
+
+            time_text.style.fg = get_text_color(workmode, minutes_remaining);
+
+            let span = Paragraph::new(time_text).alignment(Alignment::Center);
 
             let mut pie = Pie {
                 percent: Some(percent),
